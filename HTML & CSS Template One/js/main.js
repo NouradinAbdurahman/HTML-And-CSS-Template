c152abee8cdb32c074ac6234e8fee38f638ab9e1
NOURADDIN ⭐️ <70664023+NouradinAbdurahman@users.noreply.github.com>
2022-06-23 21:28:17 +0300

    JS

---
+++ HTML & CSS Template One/js/main.js
@@ -0,0 +1,7 @@
+// *----- Show Other Links Section ----*
+const otherLinks = document.querySelector(".other-link");
+const megaMenu = document.querySelector(".mega-menu");
+otherLinks.addEventListener("click", () => {
+    megaMenu.classList.toggle("show-mega-menu");
+})
+// *----- End Show Other Links Section ----*
