85e4ecb769aa751c364fd4f68374a719a0b8569b
NOURADDIN ⭐️ <70664023+NouradinAbdurahman@users.noreply.github.com>
2022-07-07 23:41:52 +0300

    Added Events section

--- HTML & CSS Template One/js/main.js
+++ HTML & CSS Template One/js/main.js
@@ -5,3 +5,34 @@ otherLinks.addEventListener("click", () => {
     megaMenu.classList.toggle("show-mega-menu");
 })
 // *----- End Show Other Links Section ----*
+
+//  *---------- start time ------------*
+let futureTime = new Date("Sep 02,2022 22:00:00").getTime();
+let counter = setInterval(() => {
+    // get date now
+    let getDate = new Date().getTime();
+    // get the diffrence between now and future time
+    let dateDiff = futureTime - getDate;
+
+    // values in ms
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneHour = 60 * 60 * 1000;
+    const oneMinute = 60 * 1000;
+
+    // get time unites
+    let days = Math.floor(dateDiff / oneDay);
+    let hours = Math.floor((dateDiff % oneDay) / oneHour);
+    let minutes = Math.floor((dateDiff % oneHour) / oneMinute);
+    let seconds = Math.floor((dateDiff % oneMinute) / 1000);
+
+    console.log(days);
+    console.log(hours);
+    console.log(minutes);
+    console.log(seconds);
+
+    const d = document.querySelector(".days").innerHTML = days;
+    const h = document.querySelector(".hours").innerHTML = hours;
+    const m = document.querySelector(".minutes").innerHTML = minutes;
+    const s = document.querySelector(".seconds").innerHTML = seconds;
+}, 1000);
+//  *---------- end time ------------*
