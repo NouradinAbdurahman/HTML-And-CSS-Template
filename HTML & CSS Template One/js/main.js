f404d7f33b38dbc4e2590ca169ba9828c6bb37e1
NOURADDIN ⭐️ <70664023+NouradinAbdurahman@users.noreply.github.com>
2022-07-10 23:51:28 +0300

    Added Pricing Section.

--- HTML & CSS Template One/js/main.js
+++ HTML & CSS Template One/js/main.js
@@ -7,12 +7,13 @@ otherLinks.addEventListener("click", () => {
 // *----- End Show Other Links Section ----*
 
 //  *---------- start time ------------*
-let futureTime = new Date("Sep 02,2022 22:00:00").getTime();
+// let futureDate = new Date("Sep 02,2022 22:00:00").getTime();
+let futureDate = new Date("Sep 02,2022 01:9:00").getTime();
 let counter = setInterval(() => {
     // get date now
     let getDate = new Date().getTime();
     // get the diffrence between now and future time
-    let dateDiff = futureTime - getDate;
+    let dateDiff = futureDate - getDate;
 
     // values in ms
     const oneDay = 24 * 60 * 60 * 1000;
@@ -25,14 +26,15 @@ let counter = setInterval(() => {
     let minutes = Math.floor((dateDiff % oneHour) / oneMinute);
     let seconds = Math.floor((dateDiff % oneMinute) / 1000);
 
-    console.log(days);
-    console.log(hours);
-    console.log(minutes);
-    console.log(seconds);
+    const d = document.querySelector(".days").innerHTML = days < 10 ? `0${days}`: days;
+    const h = document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}`: hours;
+    const m = document.querySelector(".minutes").innerHTML = minutes < 10 ? `0${minutes}`: minutes;
+    const s = document.querySelector(".seconds").innerHTML = seconds < 10 ? `0${seconds}`: seconds;
 
-    const d = document.querySelector(".days").innerHTML = days;
-    const h = document.querySelector(".hours").innerHTML = hours;
-    const m = document.querySelector(".minutes").innerHTML = minutes;
-    const s = document.querySelector(".seconds").innerHTML = seconds;
+
+    if (dateDiff < 0) {
+        clearInterval(counter);
+    }
+    
 }, 1000);
 //  *---------- end time ------------*
